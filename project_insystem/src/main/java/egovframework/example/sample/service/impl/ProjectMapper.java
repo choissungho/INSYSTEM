6d8cd69850e9f801fdda56af09103394d9bcd11f
user <user@DESKTOP-2C1OV9I>
2021-04-23 16:53:38 +0900

    최성호 - 11111

---
+++ project_insystem/src/main/java/egovframework/example/sample/service/impl/ProjectMapper.java
@@ -0,0 +1,11 @@
+package egovframework.example.sample.service.impl;
+
+import java.util.Map;
+
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+
+@Mapper("projectMapper")
+public interface ProjectMapper {
+
+}
+ 
